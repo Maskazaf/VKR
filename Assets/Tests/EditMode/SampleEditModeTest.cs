1557d9fe8ec2c2052be9da31a8e4fd2fc6f43a46
Maskazaf <maskazaf@gmail.com>
2025-05-26 20:49:25 +0300

    Добавлен EditMode тест для CI

---
+++ Assets/Tests/EditMode/SampleEditModeTest.cs
@@ -0,0 +1,11 @@
+// Assets/Tests/EditMode/SampleEditModeTest.cs
+using NUnit.Framework;
+
+public class SampleEditModeTest
+{
+    [Test]
+    public void Test_Addition_Works()
+    {
+        Assert.AreEqual(2 + 2, 4);
+    }
+}
